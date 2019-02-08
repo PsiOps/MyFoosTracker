70df4201a9afb414fdaf0cceae277e82de5bfe5a
Paul.Siersma <paul.siersma@snappet.org>
2019-02-08 15:33:19 +0100

    Match history: Show all / Show mine

--- src/app/tab2/tab2.page.ts
+++ src/app/tab2/tab2.page.ts
@@ -1,10 +1,9 @@
 import { Component } from '@angular/core';
-import { AngularFirestore, AngularFirestoreCollection } from '@angular/fire/firestore';
+import { AngularFirestore, AngularFirestoreCollection, CollectionReference, Query } from '@angular/fire/firestore';
 import { Match, MatchStatus } from '../domain/match';
-import { Observable } from 'rxjs';
+import { Observable, of } from 'rxjs';
 import { AuthenticationService } from '../auth/authentication.service';
 import { map } from 'rxjs/operators';
-import { Player } from '../domain/player';
 
 @Component({
   selector: 'app-tab2',
@@ -12,27 +11,46 @@ import { Player } from '../domain/player';
   styleUrls: ['tab2.page.scss']
 })
 export class Tab2Page {
-  public playerMatches: AngularFirestoreCollection<Match>;
-  public playerMatchesPerDay$: Observable<{day: Date, matches: Match[]}[]>;
-  constructor(private afs: AngularFirestore, private authService: AuthenticationService) {
-    this.playerMatches = afs.collection<Match>('matches',
-      ref => ref.where('status', '==', MatchStatus.over)
-        .where('participants', 'array-contains', this.authService.user.uid)
-        .orderBy('dateTimeStart', 'desc').limit(10)
-    );
-    this.playerMatchesPerDay$ = this.playerMatches.valueChanges().pipe(map(matches => {
-      return matches.reduce((result: any[], match: any) => {
-        console.log(result, match);
-        const key = match.dateTimeStart.toDate().toISOString().substring(0, 10);
-        console.log(key);
-        let resultItem = result.find(i => i.day === key);
-        if (!resultItem) {
-          resultItem = {day: key, matches: [] };
-          result.push(resultItem);
-        }
-        resultItem.matches.push(match);
-        return result;
-      }, []);
-    }));
+  public matches: AngularFirestoreCollection<Match>;
+  public matchesPerDay$: Observable<{ day: Date, matches: Match[] }[]>;
+  private showAllValue = false;
+  public get showAll() {
+    return this.showAllValue;
   }
+  public set showAll(val: boolean) {
+    this.showAllValue = val;
+    this.updateMatches(val);
+  }
+  constructor(private afs: AngularFirestore, private authService: AuthenticationService) {
+    this.updateMatches(this.showAllValue);
+  }
+  private updateMatches(showAll: boolean) {
+    this.matches = showAll ?
+      this.afs.collection<Match>('matches', ref => this.showAllFinished(ref)) :
+      this.afs.collection<Match>('matches', ref => this.showFinishedForPlayer(ref));
+    this.matchesPerDay$ = this.matches
+      .valueChanges()
+      .pipe(map(this.groupMatchesByDay));
+  }
+  private groupMatchesByDay(matches: Match[]): { day: Date, matches: Match[] }[] {
+    return matches.reduce((result: any[], match: any) => {
+      const key = match.dateTimeStart.toDate().toISOString().substring(0, 10);
+      let resultItem = result.find(i => i.day === key);
+      if (!resultItem) {
+        resultItem = { day: key, matches: [] };
+        result.push(resultItem);
+      }
+      resultItem.matches.push(match);
+      return result;
+    }, []);
+  }
+  private showAllFinished = (ref: CollectionReference): Query => ref
+    .where('status', '==', MatchStatus.over)
+    .orderBy('dateTimeStart', 'desc')
+    .limit(10)
+  private showFinishedForPlayer = (ref: CollectionReference): Query => ref
+    .where('status', '==', MatchStatus.over)
+    .where('participants', 'array-contains', this.authService.user.uid)
+    .orderBy('dateTimeStart', 'desc')
+    .limit(10)
 }
