880678cfb9bb4e01d5a2224a0e2f605fcf249174
Paul.Siersma <paul.siersma@snappet.org>
2019-04-03 19:58:32 +0200

    Introducing bulk update function (disabled)

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -9,6 +9,12 @@ import { NotificationService } from './services/notification.service';
 
 const statsUpdateService = new StatsUpdateService(firestore);
 
+const updateDocs = (docs: FirebaseFirestore.QuerySnapshot, updateObject: any) => {
+    docs.forEach(doc => {
+        doc.ref.update(updateObject).catch(err => console.log(err));
+    })
+}
+
 export const updatePlayerStats = functions.https.onCall(async (data, context) => {
     return await statsUpdateService.updateStatsForMatch(data.matchPath);
 });
@@ -26,6 +32,14 @@ export const markForRecalculation = functions.https.onRequest(async (req, res) =
     const statsRecalcService = new StatsRecalcService(statsUpdateService, firestore);
     const message = await statsRecalcService.markForRecalculation();
     res.send(message);
+});
+export const updateData = functions.https.onRequest(async (req, res) => {
+    // const fieldValue = admin.firestore.FieldValue;
+    // const playerDocs = await firestore.collection('players').get();
+    // updateDocs(playerDocs, {defaultTableRef: fieldValue.delete(), defaultTableId: 'HvPz1XQMtOGAxw0pq1dq', watchingTableIds: ['HvPz1XQMtOGAxw0pq1dq']});
+    // const matchDocs = await firestore.collection('matches').get();
+    // updateDocs(matchDocs, {tableRef: firestore.doc(`foosball-tables/HvPz1XQMtOGAxw0pq1dq`)})
+    res.send('done');
 });
 export const testNotifications = functions.https.onRequest(async (req, res) => {
     const testPlayer = (await firestore.doc(`players/SrjvBNMltdRZ2jQsV8bd6kpwIm53`).get()).data() as Player;
