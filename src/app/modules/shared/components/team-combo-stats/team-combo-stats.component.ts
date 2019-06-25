bfbca23362359228cbfdc59c26d8c4c6360b15f6
Paul.Siersma <paul.siersma@snappet.org>
2019-06-25 17:54:21 +0200

    Implemented stats overview for planned match

--- src/app/modules/shared/components/team-combo-stats/team-combo-stats.component.ts
+++ src/app/modules/shared/components/team-combo-stats/team-combo-stats.component.ts
@@ -14,17 +14,19 @@ export class TeamComboStatsComponent implements OnChanges {
 
   @Input() match: Match;
 
+  public teamComboStats$: Observable<TeamComboStats>;
   public teamAStats$: Observable<Stats>;
   public teamBStats$: Observable<Stats>;
 
   constructor(private afs: AngularFirestore, private teamService: TeamService) { }
 
   ngOnChanges() {
-    console.log('Match structure changed');
     const teamAId = this.teamService.getMatchTeamId(this.match, Team.teamA);
     const teamBId = this.teamService.getMatchTeamId(this.match, Team.teamB);
     const teamComboStatsId = this.teamService.getTeamComboId(this.match);
     const teamComboStatsDoc = this.afs.doc<TeamComboStats>(`team-combo-stats/${teamComboStatsId}`);
+
+    this.teamComboStats$ = teamComboStatsDoc.valueChanges();
 
     this.teamAStats$ = teamComboStatsDoc.valueChanges()
       .pipe(filter(s => !!s))
