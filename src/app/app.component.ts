069aa34382ec6258969850b77c23759cdb1b8df1
Paul.Siersma <paul.siersma@snappet.org>
2019-07-23 15:42:30 +0200

    Fixing the login bug for realsies now

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -4,8 +4,9 @@ import { SplashScreen } from '@ionic-native/splash-screen/ngx';
 import { StatusBar } from '@ionic-native/status-bar/ngx';
 import { UpdateService } from './services/update-service';
 import { MessagingService } from './services/messaging.service';
-import { filter, tap } from 'rxjs/operators';
+import { filter, tap, skip } from 'rxjs/operators';
 import { PlayerService } from './services/player.service';
+import { AuthenticationService } from './auth/authentication.service';
 
 @Component({
   selector: 'app-root',
@@ -17,6 +18,7 @@ export class AppComponent implements OnInit {
     private splashScreen: SplashScreen,
     private statusBar: StatusBar,
     private updateService: UpdateService,
+    private authService: AuthenticationService,
     private playerService: PlayerService,
     private messagingService: MessagingService,
     private loadingController: LoadingController
@@ -30,9 +32,12 @@ export class AppComponent implements OnInit {
         this.statusBar.styleDefault();
         this.splashScreen.hide();
       }
+      this.authService.user$
+        .pipe(skip(1))
+        .subscribe(async user => {
+          await this.loadingController.dismiss();
+        });
       this.playerService.player$
-        .pipe(filter(player => !!player))
-        .pipe(tap(() => this.loadingController.dismiss()))
         .subscribe(player => {
           this.messagingService.requestPermission(player);
           this.messagingService.monitorTokenRefresh(player);
