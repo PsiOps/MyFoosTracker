29642ae33d1b7a8ecfe3fa3949e68c722408a419
Paul.Siersma <paul.siersma@snappet.org>
2019-07-25 07:40:36 +0200

    Better initial loader flow

--- src/app/auth/auth.guard.ts
+++ src/app/auth/auth.guard.ts
@@ -1,22 +1,24 @@
 import { Injectable } from '@angular/core';
 import { CanActivate, ActivatedRouteSnapshot, RouterStateSnapshot, Router } from '@angular/router';
 import { Observable } from 'rxjs';
-import { map, tap } from 'rxjs/operators';
+import { map, tap, skip } from 'rxjs/operators';
 import { AuthenticationService } from './authentication.service';
 
 @Injectable({
   providedIn: 'root'
 })
 export class AuthGuard implements CanActivate {
-  constructor(private authService: AuthenticationService, private router: Router) {}
+  constructor(private authService: AuthenticationService, private router: Router) { }
   canActivate(
     next: ActivatedRouteSnapshot,
     state: RouterStateSnapshot): Observable<boolean> | Promise<boolean> | boolean {
-      return this.authService.user$.pipe(map(u => u != null), tap(isLoggedIn => {
+    return this.authService.user$
+      .pipe(skip(1))
+      .pipe(map(u => u != null), tap(isLoggedIn => {
         if (!isLoggedIn) {
           this.router.navigateByUrl('/login');
         }
       }
-    ));
+      ));
   }
 }
