a8a8874e049ba288e80e0cac401fc2780067ab0d
Paul.Siersma <paul.siersma@snappet.org>
2019-09-18 08:37:36 +0200

    No group component to show loader while group is being loaded

--- src/app/modules/shared/components/no-group/no-group.component.ts
+++ src/app/modules/shared/components/no-group/no-group.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit, Input } from '@angular/core';
+import { Component, Input } from '@angular/core';
 import { ModalController } from '@ionic/angular';
 import { GroupService } from 'src/app/services/group.service';
 import { GroupModalComponent } from '../group-modal/group-modal.component';
@@ -9,16 +9,21 @@ import { Player } from 'src/app/domain';
   templateUrl: './no-group.component.html',
   styleUrls: ['./no-group.component.scss'],
 })
-export class NoGroupComponent implements OnInit {
+export class NoGroupComponent {
 
+  public isLoading = true;
   @Input() player: Player;
+  @Input() set groupId(groupId: string) {
+    if (groupId === null) {
+      // Only show the no group message (instead of a loader) if the incoming currentGroupId is null
+      this.isLoading = false;
+    }
+  }
 
   constructor(
     private modalController: ModalController,
     private groupService: GroupService
   ) { }
-
-  ngOnInit() { }
 
   public async createGroup() {
     await this.groupService.addGroupToPlayer(this.player.id);
